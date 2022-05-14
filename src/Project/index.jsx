118bef6f7d6e58a1bb573be3f1e269d16ef81571
Zuzike Nemesová <darigjorkinen@gmail.com>
2022-05-14 22:31:34 +0200

    Vytvorenie podkladu komponent

---
+++ src/Project/index.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import img from './img/photo1.jpg';
+
+export const Project = () => {
+  return (
+    <div class="kontejner">
+      <header>
+        <h1>Trefa na kôš</h1>
+        <p>projekt Zuzky Nemesovej a Ivi Plašienkovej</p>
+      </header>
+      <div class="navigace">
+        <button type="menu">O nás</button>
+        <button type="menu">O projekte</button>
+      </div>
+      <div class="uvod">
+        <h1>My dve</h1>
+        <img id="my_dve" src={img} alt="Zuzka a Iva" />
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis,
+          eaque? Aliquid est nesciunt atque. Ducimus eum voluptatem
+          necessitatibus consequatur! Autem, unde. Rerum consequuntur
+          dignissimos omnis rem at id nemo amet?Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Numquam corporis iste dolor animi,
+          reprehenderit ipsam corrupti magni quaerat fuga sunt amet voluptatem!
+          Ipsam aut rerum aspernatur doloremque minus atque dolore?
+        </p>
+      </div>
+    </div>
+  );
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,31 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { Project } from './Project';
 import './style.css';
 
-const App = () => (
-  <div class="kontejner">
-    <header>
-      <h1>Trefa na kôš</h1>
-      <p>projekt Zuzky Nemesovej a Ivi Plašienkovej</p>
-    </header>
-    <div class="navigace">
-      <button type="menu">O nás</button>
-      <button type="menu">Trefa na kôš</button>
-    </div>
-    <div class="uvod">
-      <h1>My dve</h1>
-      <img id="my_dve" src="img/zuzka_a_iva.jpg" alt="Zuzka a Iva" />
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis,
-        eaque? Aliquid est nesciunt atque. Ducimus eum voluptatem necessitatibus
-        consequatur! Autem, unde. Rerum consequuntur dignissimos omnis rem at id
-        nemo amet?Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        Numquam corporis iste dolor animi, reprehenderit ipsam corrupti magni
-        quaerat fuga sunt amet voluptatem! Ipsam aut rerum aspernatur doloremque
-        minus atque dolore?
-      </p>
-    </div>
-  </div>
-);
+const App = () => <Project />;
 
 createRoot(document.querySelector('#app')).render(<App />);
