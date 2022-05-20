1de651b287edf29e9ac5c70a82565427c638bb3a
Ivana Plašienková <ivana.plasienkova@gmail.com>
2022-05-20 19:31:37 +0200

    Inštalácia knižnice React Router, pokusy o nastavenie linkov

--- src/Domov/index.jsx
+++ src/Domov/index.jsx
@@ -5,6 +5,7 @@ import './style.css';
 export const Domov = () => {
   return (
     <div className="domov">
+      <Menu />
       <header className="domov__hlavicka">
         Vitajte na našich mikrostránkach
       </header>
@@ -13,18 +14,6 @@ export const Domov = () => {
         rámci akadémie Czechitas. Môžete sa dozvedieť aj o nás, inšpiráciách a
         čo za tým všetkým je.
       </p>
-      <Menu />
-      {/* <div className="main__navigace">
-        <button className="btn__nav btn--domov"></button>
-        <p>Domov</p>
-        <button className="btn__nav btn--projekt"></button>
-        <p>O projekte</p>
-        <button className="btn__nav btn--my"></button>
-        <p>O nás</p>
-        <a href="">Domov <i class=""></i></a>
-        <a href="">O projekte</a>
-  <a href="">O nás</a>
-      </div>*/}
     </div>
   );
 };
--- src/index.jsx
+++ src/index.jsx
@@ -5,9 +5,51 @@ import { Menu } from './Menu';
 import { Domov } from './Domov';
 import { Projekt } from './Projekt';
 import './style.css';
+import { BrowserRouter, Routes, Route, Link, Outlet } from 'react-router-dom';
 
-const App = () => {
-  return <Projekt />;
+export const App = () => {
+  return (
+    <>
+      <div className="Menu__navigace">
+        <Link to="/Menu__navigace--domov">
+          <div className="Menu__navigace--domov">
+            {' '}
+            |<button className="btn__nav btn--domov"></button>
+            <p>Domov</p>
+          </div>
+        </Link>
+
+        <Link to="/Menu__navigace--o__projekte">
+          <div className="Menu__navigace--o__projekte">
+            <button className="btn__nav btn--o__projekte"></button>
+            <p>O projekte</p>
+          </div>
+        </Link>
+
+        <Link to="/Menu__navigace--o__nas">
+          <div className="Menu__navigace--o__nas">
+            <button className="btn__nav btn--o_nas"></button>
+            <p>O nás</p>
+          </div>
+        </Link>
+
+        {/*<a href="">Domov <i class=""></i></a>
+<a href="">O projekte</a>
+<a href="">O nás</a>*/}
+      </div>
+      <Outlet />
+    </>
+  );
 };
 
-createRoot(document.querySelector('#app')).render(<App />);
+createRoot(document.querySelector('#app')).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/Menu__navigace" element={<Menu />}>
+        <Route path="/Menu__navigace--domov" element={<Domov />} />
+        <Route path="/Menu__navigace--o__nas" element={<ONas />} />
+        <Route path="/Menu__navigace--o__projekte" element={<Projekt />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>,
+);
