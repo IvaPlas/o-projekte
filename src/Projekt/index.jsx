0748555f5fc6291b0513e7e3e7f61a43e1182df3
Zuzike Nemesová <darigjorkinen@gmail.com>
2022-05-22 14:08:40 +0200

    Dopísanie textov o projekte

--- src/Projekt/index.jsx
+++ src/Projekt/index.jsx
@@ -13,29 +13,48 @@ export const Projekt = () => {
       <div className="projekt__opis">
         <h2>O čom že ten náš projekt je?</h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet totam
-          vel ratione voluptatibus, accusantium fugit, iusto cupiditate,
-          doloribus ex debitis minima deleniti! Illum repellat, consectetur esse
-          maiores veritatis quidem accusamus. Lorem ipsum dolor sit amet
-          consectetur adipisicing elit. Consequatur, dolores natus eligendi cum
-          labore similique! Itaque dolore assumenda tenetur corporis deserunt
-          harum temporibus facere hic, molestias officiis fugiat sunt aliquid?
+          Natrafili sme na skvelo vypracovanú príručku "Alchýmia triedenia
+          komunálneho odpadu" k recyklácii odpadov na Slovensku, ktorú vytvorili
+          dve študentky v spolupráci s Inštitútom cirkulárnej ekonomiky a
+          organizáciou zodpovednosti výrobcov NATUR-PACK. Príručka je dostupná
+          iba v .pdf formáte a preto nás napadlo vytvoriť interaktívnejšiu,
+          online verziu. Asi to poznáte, že aj ako svedomití recyklátori, často
+          dlho rozmýšľame, kam náš “odpad” správne vytriediť a nakoniec si ani
+          nie sme istí, či sme nenarobili viac škody než úžitku. Preto by sme
+          chceli, aby stránky fungovali ako <strong>vyhľadávač</strong>, ktorý
+          po zadaní názvu toho, čo chceme vyhodiť, nás na základe kľúčových slov
+          nasmeruje k riešeniu. Užívatelia by si tak nemuseli čítať dlhé manuály
+          a mohli by mať stránky/vyhľadávač viac po ruke. Uľahčením prístupu k
+          cieľovým informáciám by sme chceli tiež ľudí podporiť v recyklovaní.
+          Na stránkach sa bude dať dočítať aj viac podrobnejších a motivujúcich
+          faktov. Tiež by sme boli radi, keby sa z tohoto projektu neskôr
+          vyvinula praktická aplikácia.
         </p>
         <h2>Ako na tom pracujeme</h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
-          tempore placeat culpa a numquam vitae quidem impedit aspernatur
-          laboriosam omnis? Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Velit nostrum officia, culpa eveniet placeat quas accusantium
-          neque harum saepe aut?
+          Chvíľu sme si dopisovali a zdieľali nápady. Mali sme jedno veľké
+          stretnutie pri kávičke a skvelých avokádových chlebíčkoch, pri ktorom
+          sme poskladali ako by sme boli rady, aby stránky vyzereli, čo všetko
+          by mali obsahovať. Na diaľku sme brainstormovali o názve. Nedávno nám
+          priradili už mentora Paľa, ktorý je tiež zo Slovenska a už sme mali
+          prvé výživné stretko.Takže máme už konkrétnejšie úlohy a na ne 2
+          týždne, aby sme si predpripravili, čo sa len bude dať do hackatonu -
+          kde to celé zbuchneme :D.
         </p>
         <h2>Aké sú naše plány s projektom</h2>
         <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Expedita
-          tempore placeat culpa a numquam vitae quidem impedit aspernatur
-          laboriosam omnis? Lorem ipsum dolor sit amet consectetur adipisicing
-          elit. Velit nostrum officia, culpa eveniet placeat quas accusantium
-          neque harum saepe aut?
+          Boli by sme radi, keby sa dá urobiť aj aplikácia, ktorú by bolo možno
+          použiť aj v iných krajinách, že by sa iba obmieňal aktuálny obsah.
+          Bolo by fajn prepojenie už s inými existujícimi platformami, ktoré
+          napríklad mapujú umiestnenie košov a podobne alebo pridať vlastné
+          mapy. Určite by bolo potrebná neustála aktualizácia, ktorá reaguje na
+          zmeny v legislatíve. V divokejšej verzii si predstavujeme, že by bolo
+          fajn appku trocha gamifikovať. Vytvoriť platformu, kde by sa ľudia
+          bavili s tým, že by zaznamenávali, čo aj reálne vyhodia do košov a
+          získavali by zato body. Mohli by diskutovať a zdielať zistenia.
+          Vlastne by to nebola len pomôcka pre tých, ktorý sa o recykláciu
+          zaujímajú, ale v nejakej atraktívnej vizuálnej podobe, by to mohlo
+          prilákať k recyklovaniu aj ľudí, ktorích to dovtedy nezaujímalo.
         </p>
       </div>
     </div>
